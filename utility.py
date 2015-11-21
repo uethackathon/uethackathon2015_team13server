4fb9dbed958b289e885d8f23609b1f055bcbec1d
cat <caty.banimal.03@gmail.com>
2015-11-22 02:17:38 +0700

    Progress update

--- utility.py
+++ utility.py
@@ -6,6 +6,7 @@
 from collections import Counter
 import xml.etree.ElementTree as ET
 import subprocess
+import time
 
 
 class TagParser():
@@ -13,41 +14,49 @@ class TagParser():
     Extract tagged data after using Tagger library for Vietnamese sentences
     """
 
-    _popular_data = Counter()
-    _tag_list = {
-        'Np': 'Proper noun',
-        'Nc': 'Classifier',
-        'Nu': 'Unit noun',
-        'N': 'Common noun',
-        'V': 'Verb',
-        'A': 'Adjective',
-        'P': 'Pronoun',
-        'R': 'Adverb',
-        'L': 'Determiner',
-        'M': 'Numeral',
-        'E': 'Preposition',
-        'C': 'Subordinating conjunction',
-        'CC': 'Coordinating conjunction',
-        'I': 'Interjection',
-        'T': 'Auxiliary, modal words',
-        'Y': 'Abbreviation',
-        'Z': 'Bound morphemes',
-        'X': 'Unknown'
-    }
-    _bag_of_tagged_word = {}
-    _wordlist = {}
+
 
     # =========================================
     def __init__(self, popular=None, bag_of_tagged_word=None, wordlist=None):
         """Constructor for TagParser"""
+
+        self._tag_list = {
+            'Np': 'Proper noun',
+            'Nc': 'Classifier',
+            'Nu': 'Unit noun',
+            'N': 'Common noun',
+            'V': 'Verb',
+            'A': 'Adjective',
+            'P': 'Pronoun',
+            'R': 'Adverb',
+            'L': 'Determiner',
+            'M': 'Numeral',
+            'E': 'Preposition',
+            'C': 'Subordinating conjunction',
+            'CC': 'Coordinating conjunction',
+            'I': 'Interjection',
+            'T': 'Auxiliary, modal words',
+            'Y': 'Abbreviation',
+            'Z': 'Bound morphemes',
+            'X': 'Unknown'
+        }
+
         if popular is not None:
             self._popular_data = popular
+        else:
+            self._popular_data = Counter()
 
         if bag_of_tagged_word is not None:
             self._bag_of_tagged_word = bag_of_tagged_word
+        else:
+            self._bag_of_tagged_word = {}
 
         if wordlist is not None:
             self._wordlist = wordlist
+        else:
+            self._wordlist = {}
+
+        self.sentences = []
 
     #======================================
     def _add_bag(self, tagged_word_list=None):
@@ -63,14 +72,17 @@ class TagParser():
                 self._bag_of_tagged_word[word['tag']] = []
 
             # Insert a word into _
-            if word['word'] not in self._bag_of_tagged_word:
-                self._bag_of_tagged_word[word['tag']].append(word['word'])
+            # if word['word'] not in self._bag_of_tagged_word:
+            self._bag_of_tagged_word[word['tag']].append(word['word'])
 
             # Count the word to find the popular of word
             self._popular_data[word['word']] += 1
 
             # Word to tag mapping
             self._wordlist[word['word']] = word['tag']
+
+            # Add vocabulary
+            # self._vocabulary.append(word['word'])
 
         return True
 
@@ -85,7 +97,7 @@ class TagParser():
 
         """
         wordlist_tagged = []
-        words = input_sentence.split(' ')
+        words = input_sentence.strip().split(' ')
         for word in words:
             word, tag = word.split('/')
             if len(word) > 1 and self._tag_list.has_key(tag):
@@ -105,7 +117,7 @@ class TagParser():
         :param input_sentence:
 
         """
-        tagged_words = self.extract_word_tags(input_sentence)
+        tagged_words = self.extract_word_tags_raw_sentence(input_sentence)
         self._add_bag(tagged_words)
 
     # ======================================
@@ -115,11 +127,16 @@ class TagParser():
         wordlist_tagged = []
         root = doc.getroot()
         for sentence in root.findall('s'):
+            conjunction = []
             for word in sentence.findall('w'):
                 wordlist_tagged.append({
                     'tag': word.get('pos'),
                     'word': word.text
                 })
+                conjunction.append(word.text)
+            print conjunction
+            print ' '.join(conjunction)
+            self.sentences.append(' '.join(conjunction))
 
         return wordlist_tagged
 
@@ -136,17 +153,32 @@ class TagParser():
 
 
 
+
 class NLPLibrary:
     """"""
 
     #=========================================
     def __init__(self):
         """Constructor for NLPLibrary"""
+        self.TP = TagParser()
 
     #======================================
-    def add_tag(self, file_input="", file_output=""):
+    def _add_tag(self, file_input="", file_output=""):
         # IMPORTANT: The input and output file MUST be absolute path
-
+        if file_output == "":
+            file_output = '/tmp/out.smartfeedback'
         subprocess.call(['./vnTagger.sh', '-i %s -uo %s' % (file_input, file_output)])
+        self.TP.add_bag_xml_file(file_output)
+
+    #======================================
+    def tag_paragraph(self, paragraph=""):
+        # Write paragraph to file prepare for Tagger
+        filepath = '/tmp/%sparagraph.input.smartfeedback' % (str(round(time.time())))
+        f = open(filepath, 'w')
+        f.write(paragraph)
+        f.close()
+        self._add_tag(file_input=filepath)
+
+
 
 
